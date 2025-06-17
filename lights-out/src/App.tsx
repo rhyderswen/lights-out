565ed52c8041469f6825f32389143b7616f255b6
Rembi <rembitime@gmail.com>
2025-06-17 16:57:17 -0600

    Reformat code with Prettier

--- lights-out/src/App.tsx
+++ lights-out/src/App.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useState } from 'react'
-import './App.css'
-import Header from './components/Header';
-import GameArea from './components/GameArea';
-import ConfettiExplosion from 'react-confetti-explosion';
-import Popup from 'reactjs-popup';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import GameArea from "./components/GameArea";
+import ConfettiExplosion from "react-confetti-explosion";
+import Popup from "reactjs-popup";
 
 function App() {
   const [confettiActive, setConfettiActive] = useState(false);
@@ -12,23 +12,27 @@ function App() {
   function incrementMoves() {
     setMoves(moves + 1);
   }
-  
+
   const columns = 5; // Note: Valid solution checking is only implemented for 5x5. There will be errors if this is changed
   const rows = 5;
   const [tileStates, setTileStates] = useState<Array<boolean>>([]);
   function flipTile(row: number, column: number) {
     incrementMoves();
-    const clicked = row*columns + column;
+    const clicked = row * columns + column;
     const updatedTiles = tileStates.map((state, i) => {
       if (i === clicked) {
         return !state;
-      } else if (clicked % columns !== 0 && i+1 === clicked) { // Switch left square if it the clicked square isn't on the left edge
+      } else if (clicked % columns !== 0 && i + 1 === clicked) {
+        // Switch left square if it the clicked square isn't on the left edge
         return !state;
-      } else if (clicked % columns !== columns-1 && i-1 === clicked) { // Switch right square if it the clicked square isn't on the right edge
+      } else if (clicked % columns !== columns - 1 && i - 1 === clicked) {
+        // Switch right square if it the clicked square isn't on the right edge
         return !state;
-      } else if (clicked / columns >= 1 && i+columns === clicked) { // Switch upper square if it the clicked square isn't on the top edge
+      } else if (clicked / columns >= 1 && i + columns === clicked) {
+        // Switch upper square if it the clicked square isn't on the top edge
         return !state;
-      } else if (clicked / columns < rows-1 && i-columns === clicked) { // Switch lower square if it the clicked square isn't on the bottom edge
+      } else if (clicked / columns < rows - 1 && i - columns === clicked) {
+        // Switch lower square if it the clicked square isn't on the bottom edge
         return !state;
       } else {
         return state;
@@ -38,13 +42,19 @@ function App() {
     checkForWin(updatedTiles);
   }
 
-  const checkIfSolvable = useCallback((arr: boolean[])  => {
-    const n1 = [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0]; // from "Turning Lights Out with Linear Algebra" by Marlow Anderson and Todd Feil
-    const n2 = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1];
+  const checkIfSolvable = useCallback((arr: boolean[]) => {
+    const n1 = [
+      0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0,
+    ]; // from "Turning Lights Out with Linear Algebra" by Marlow Anderson and Todd Feil
+    const n2 = [
+      1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1,
+    ];
 
     function dotProduct(a: number[], b: number[]) {
       if (a.length !== b.length) {
-        console.error("Tried to do dot product on vectors of different length!");
+        console.error(
+          "Tried to do dot product on vectors of different length!"
+        );
         return NaN;
       }
       let sum = 0;
@@ -54,26 +64,26 @@ function App() {
       return sum;
     }
 
-    const numArr = arr.map(state => +state);
-      console.log(numArr);
+    const numArr = arr.map((state) => +state);
+    console.log(numArr);
     if (dotProduct(numArr, n1) % 2 === 0 && dotProduct(numArr, n2) % 2 === 0) {
       console.log("solvable!\n");
       return true;
     }
-      console.log("not solvable!\n");
+    console.log("not solvable!\n");
     return false;
   }, []);
 
   const randomizeTiles = useCallback(() => {
-    const arr = Array(rows*columns);
+    const arr = Array(rows * columns);
     do {
-      for (let i = 0; i < rows*columns; i++) {
+      for (let i = 0; i < rows * columns; i++) {
         arr[i] = Boolean(Math.round(Math.random())); // Generates a random boolean
       }
     } while (!checkIfSolvable(arr));
     setTileStates(arr);
   }, [checkIfSolvable]);
-  
+
   useEffect(() => {
     randomizeTiles();
   }, [randomizeTiles]);
@@ -89,7 +99,7 @@ function App() {
   }
 
   function checkForWin(tiles: boolean[]) {
-    if (tiles.every(state => !state)) {
+    if (tiles.every((state) => !state)) {
       setConfettiActive(false);
       setTimeout(() => setConfettiActive(true), 0);
       setOpenWinPopup(true);
@@ -98,21 +108,42 @@ function App() {
 
   return (
     <>
-      {confettiActive && <ConfettiExplosion className='confetti' onComplete={() => setConfettiActive(false)}/>}
-      <Popup open={openWinPopup} closeOnDocumentClick onClose={() => setOpenWinPopup(false)}>
+      {confettiActive && (
+        <ConfettiExplosion
+          className="confetti"
+          onComplete={() => setConfettiActive(false)}
+        />
+      )}
+      <Popup
+        open={openWinPopup}
+        closeOnDocumentClick
+        onClose={() => setOpenWinPopup(false)}
+      >
         <div className="winPopup">
           You won with {moves} moves! Congrats!
           <div className="popupButtons">
-            <button className="button resetButton" onClick={closeAndReset}>Reset</button>
-            <button className="button closeButton" onClick={() => setOpenWinPopup(false)}>Close</button>
+            <button className="button resetButton" onClick={closeAndReset}>
+              Reset
+            </button>
+            <button
+              className="button closeButton"
+              onClick={() => setOpenWinPopup(false)}
+            >
+              Close
+            </button>
           </div>
         </div>
       </Popup>
-      <Header moves={moves} reset={reset}/>
-      <hr className="seperator"/>
-      <GameArea rows={rows} columns={columns} tileStates={tileStates} flipTile={flipTile}/>
+      <Header moves={moves} reset={reset} />
+      <hr className="seperator" />
+      <GameArea
+        rows={rows}
+        columns={columns}
+        tileStates={tileStates}
+        flipTile={flipTile}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- lights-out/src/components/GameArea/GameArea.tsx
+++ lights-out/src/components/GameArea/GameArea.tsx
@@ -2,20 +2,30 @@ import Tile from "../Tile";
 import "./GameArea.css";
 
 interface IGameArea {
-  rows: number,
-  columns: number,
-  tileStates: Array<boolean>,
-  flipTile: (row: number, column: number) => void
+  rows: number;
+  columns: number;
+  tileStates: Array<boolean>;
+  flipTile: (row: number, column: number) => void;
 }
 
 const GameArea = ({ rows, columns, tileStates, flipTile }: IGameArea) => {
-  
-
   return (
-    <div className="tileContainer" style={{gridTemplateColumns: "1fr ".repeat(columns), maxWidth: columns*120}}>
-      {[...Array(columns*rows)].map((_, i) => <Tile isLit={tileStates[i]} onClick={() => flipTile(Math.floor(i/columns), i%columns)} key={i}/>)}
+    <div
+      className="tileContainer"
+      style={{
+        gridTemplateColumns: "1fr ".repeat(columns),
+        maxWidth: columns * 120,
+      }}
+    >
+      {[...Array(columns * rows)].map((_, i) => (
+        <Tile
+          isLit={tileStates[i]}
+          onClick={() => flipTile(Math.floor(i / columns), i % columns)}
+          key={i}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default GameArea;
