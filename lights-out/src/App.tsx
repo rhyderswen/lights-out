8ee74bd37f096205866abf09223f39975ad0ae38
Chris Manning <chris.manning@viget.com>
2025-06-18 09:35:43 -0400

    Add `hasWon` boolean, remove confetti state
    Another potential option for the popup and confetti display
    
    * Add `useEffect` to track the moves and tileStates
    * TEMP: add "Trigger Win" button to test the popup (ignore or remove)

--- lights-out/src/App.tsx
+++ lights-out/src/App.tsx
@@ -1,90 +1,93 @@
-import { useCallback, useEffect, useState } from 'react'
-import './App.css'
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
 import Header from './components/Header';
 import GameArea from './components/GameArea';
 import ConfettiExplosion from 'react-confetti-explosion';
 import Popup from 'reactjs-popup';
 
 function App() {
-  const [confettiActive, setConfettiActive] = useState(false);
-  const [openWinPopup, setOpenWinPopup] = useState(false);
+  const [hasWon, setHasWon] = useState(false);
   const [moves, setMoves] = useState(0);
   function incrementMoves() {
     setMoves(moves + 1);
   }
-  
+
   const columns = 5;
   const rows = 5;
   const [tileStates, setTileStates] = useState<Array<boolean>>([]);
   function flipTile(row: number, column: number) {
     incrementMoves();
-    const clicked = row*columns + column;
+    const clicked = row * columns + column;
     const updatedTiles = tileStates.map((state, i) => {
       if (i === clicked) {
         return !state;
-      } else if (clicked % columns !== 0 && i+1 === clicked) { // Switch left square if it the clicked square isn't on the left edge
+      } else if (clicked % columns !== 0 && i + 1 === clicked) {
+        // Switch left square if it the clicked square isn't on the left edge
         return !state;
-      } else if (clicked % columns !== columns-1 && i-1 === clicked) { // Switch right square if it the clicked square isn't on the right edge
+      } else if (clicked % columns !== columns - 1 && i - 1 === clicked) {
+        // Switch right square if it the clicked square isn't on the right edge
         return !state;
-      } else if (clicked / columns >= 1 && i+columns === clicked) { // Switch upper square if it the clicked square isn't on the top edge
+      } else if (clicked / columns >= 1 && i + columns === clicked) {
+        // Switch upper square if it the clicked square isn't on the top edge
         return !state;
-      } else if (clicked / columns < rows-1 && i-columns === clicked) { // Switch lower square if it the clicked square isn't on the bottom edge
+      } else if (clicked / columns < rows - 1 && i - columns === clicked) {
+        // Switch lower square if it the clicked square isn't on the bottom edge
         return !state;
       } else {
         return state;
       }
     });
     setTileStates(updatedTiles);
-    checkForWin(updatedTiles);
   }
 
   const randomizeTiles = useCallback(() => {
-    const arr = Array(rows*columns);
-    for (let i = 0; i < rows*columns; i++) {
+    const arr = Array(rows * columns);
+    for (let i = 0; i < rows * columns; i++) {
       arr[i] = Boolean(Math.round(Math.random())); // Generates a random boolean
     }
     setTileStates(arr);
   }, []);
-  
+
   useEffect(() => {
     randomizeTiles();
   }, [randomizeTiles]);
 
   function reset() {
+    setHasWon(false);
     setMoves(0);
     randomizeTiles();
   }
 
-  function closeAndReset() {
-    reset();
-    setOpenWinPopup(false);
-  }
-
-  function checkForWin(tiles: boolean[]) {
-    if (tiles.every(state => !state)) {
-      setConfettiActive(false);
-      setTimeout(() => setConfettiActive(true), 0);
-      setOpenWinPopup(true);
+  useEffect(() => {
+    if (moves > 1 && tileStates.every((state) => !state)) {
+      setHasWon(true);
     }
-  }
+  }, [moves, tileStates]);
 
   return (
     <>
-      {confettiActive && <ConfettiExplosion className='confetti' onComplete={() => setConfettiActive(false)}/>}
-      <Popup open={openWinPopup} closeOnDocumentClick onClose={() => setOpenWinPopup(false)}>
+      <button onClick={() => setHasWon(true)}>Trigger Win</button>
+      {hasWon && <ConfettiExplosion className="confetti" />}
+      <Popup open={hasWon} closeOnDocumentClick onClose={reset}>
         <div className="winPopup">
           You won with {moves} moves! Congrats!
           <div className="popupButtons">
-            <button className="button resetButton" onClick={closeAndReset}>Reset</button>
-            <button className="button closeButton" onClick={() => setOpenWinPopup(false)}>Close</button>
+            <button className="button resetButton" onClick={reset}>
+              Reset
+            </button>
           </div>
         </div>
       </Popup>
-      <Header moves={moves} reset={reset}/>
-      <hr className="seperator"/>
-      <GameArea rows={rows} columns={columns} tileStates={tileStates} flipTile={flipTile}/>
+      <Header moves={moves} reset={reset} />
+      <hr className="seperator" />
+      <GameArea
+        rows={rows}
+        columns={columns}
+        tileStates={tileStates}
+        flipTile={flipTile}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
